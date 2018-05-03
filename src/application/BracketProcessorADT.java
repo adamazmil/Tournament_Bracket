65ee27a2d6f8323ef7ccbe3828ee554e9bbb4afb
Adam Azmil <36519636+adamazmil@users.noreply.github.com>
2018-05-03 16:19:49 -0500

    nothing

--- src/application/BracketProcessorADT.java
+++ src/application/BracketProcessorADT.java
@@ -5,8 +5,7 @@ import javafx.scene.layout.HBox;
 public interface BracketProcessorADT {
     public Team[] seed();
 
-    public void advanceRound(Team team1, Team team2, int round, int gameIndex); 
+    public void advanceRound(Team team1, Team team2, int round, int gameIndex);
     
     public Team[] getData(int index);
-    
 }
