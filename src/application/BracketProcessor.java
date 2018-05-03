ed3da34ef109c8bc3c369b8cfdcf2ec47ea67178
John Chen <jchen678@wisc.edu>
2018-05-03 13:17:53 -0500

    more conflict fixes

--- src/application/BracketProcessor.java
+++ src/application/BracketProcessor.java
@@ -66,8 +66,6 @@ public class BracketProcessor implements BracketProcessorADT {
             e.printStackTrace();
         }
     }
-
-<<<<<<< HEAD
     public Team[] seed() {
       //seed variables
         Team[] teamList = teamRounds[0];
@@ -89,104 +87,6 @@ public class BracketProcessor implements BracketProcessorADT {
                 teamSeed[roundItterator++]= teamList[(numberOfTeams/4)*3+i];
                 teamSeed[roundItterator++]= teamList[numberOfTeams/4+i];
                 teamSeed[roundItterator++]= teamList[(numberOfTeams/4)*3-1-i];
-=======
-	/**
-	 * Constructor for this class. Initializes instances variables to set the
-	 * starting state of the object
-	 */
-	public BracketProcessor(String filepath) {
-		try {
-			// creates stream from inputted filepath and filters to lower case and removes
-			// null/empty strings\
-			Stream<String> teamStream = Files.lines(Paths.get(filepath))
-					// remove any null or empty Strings from the Stream
-					.filter(x -> x != null && !x.equals(""))
-					// make all of the Strings in the Stream uppercase
-					.map(String::toLowerCase);
-			String[] teamsAsString = teamStream.toArray(String[]::new);
-			Team[] teamList = new Team[teamsAsString.length];
-			
-			if (teamsAsString.length == 0) {
-			    teamRounds = null;
-			    return;
-			}
-			if (teamsAsString.length == 1) {
-			    teamRounds = new Team[1][1];
-			    teamRounds[0][0] = new Team(teamsAsString[0]);
-			    return;
-			}
-			
-			// copys stream to array 1:1 (no seeding)
-			for (int j = 0; j < teamList.length; j++) {
-				teamList[j] = new Team(teamsAsString[j]);
-			}
-			teamRounds = new Team[(int) (Math.log(teamList.length) / Math.log(2)) + 1][];
-			// System.out.println((int)(Math.log(teamList.length)/Math.log(2)));
-			for (int i = 0; i < teamRounds.length; i++) {
-				if (i == 0) {
-					teamRounds[i] = teamList;
-				}
-				else {
-					teamRounds[i] = new Team[teamList.length / (int) (Math.pow(2, i))];
-					for(int j = 0; j < teamRounds[i].length; j++) {
-					    teamRounds[i][j] = new Team("TBD");
-					}
-				}
-			}
-			
-			this.numberOfTeams = teamList.length;
-			//seed variables
-        	int midPoint1 = numberOfTeams/2-1;
-        	int midPoint2 = numberOfTeams/2;
-        	int startPoint = 0;
-        	int endPoint = numberOfTeams-1;
-        	int roundItterator=0;
-        	
-        	Team [] teamSeed = new Team[numberOfTeams];
-        	if(numberOfTeams>=8) {
-        		for(int i=0; i<numberOfTeams/8;i++) {
-            		
-            		teamSeed[roundItterator++]= teamList[startPoint+i];
-            		teamSeed[roundItterator++]= teamList[endPoint-i];
-            		teamSeed[roundItterator++]= teamList[midPoint1-i];
-            		teamSeed[roundItterator++]= teamList[midPoint2+i];
-            		teamSeed[roundItterator++]= teamList[numberOfTeams/4-1-i];
-            		teamSeed[roundItterator++]= teamList[(numberOfTeams/4)*3+i];
-            		teamSeed[roundItterator++]= teamList[numberOfTeams/4+i];
-            		teamSeed[roundItterator++]= teamList[(numberOfTeams/4)*3-1-i];
-
-            	}
-        		teamRounds[0]=teamSeed;
-        	}
-        	else if(numberOfTeams==4){
-        		for(int i=0; i<numberOfTeams/4;i++) {
-            		
-            		teamSeed[roundItterator++]= teamList[startPoint+i];
-            		teamSeed[roundItterator++]= teamList[endPoint-i];
-            		teamSeed[roundItterator++]= teamList[midPoint1-i];
-            		teamSeed[roundItterator++]= teamList[midPoint2+i];
-            		
-
-            	}
-        		teamRounds[0]=teamSeed;
-        	}
-        	
-			//seed();
-			for (int i = 0; i < teamRounds.length; i++) {
-				for (int j = 0; j < teamRounds[i].length; j++) {
-					if (teamRounds[i][j] == null) {
-						System.out.print("null");
-					} else {
-						System.out.print(teamRounds[i][j].getNameString());
-					}
-				}
-				System.out.println();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
->>>>>>> 1b831a23138ad5e64274cf3a93864b8933ddc329
 
             }
         }
