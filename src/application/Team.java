d5d83cc7754f12c2ce7e547b9c66f4cff1112644
John Chen <jchen678@wisc.edu>
2018-05-03 16:06:28 -0500

    comments for team

--- src/application/Team.java
+++ src/application/Team.java
@@ -2,6 +2,9 @@ package application;
 
 import javafx.scene.control.Label;
 
+/*
+ * Team class that stores score and a label containing the team name
+ */
 public class Team {
     private Label name;
     private int score;
@@ -10,23 +13,48 @@ public class Team {
         this.name = new Label(name);
         this.score = -1;
     }
-
+    
+    /*
+     * gets label associated with team
+     * 
+     * @return  label connecting to team
+     */
     public Label getNameLabel() {
     	return name;
     }
     
+    /*
+     * sets the text of name label
+     * 
+     * @param   name    name to change label to
+     */
     public void setNameLabel(String name) {
         this.name.setText(name);;
     }
     
+    /*
+     * getter for string representation of name
+     * 
+     * @return name as a string
+     */
     public String getNameString() {
     	return name.getText();
     }
 
+    /*
+     * setter for score of team
+     * 
+     * @param   score   new score of team
+     */
     public void setScore(int score) {
         this.score = score;
     }
-
+    
+    /*
+     * getter for score
+     * 
+     * @return  int score of team
+     */
     public int getScore() {
         return score;
     }
